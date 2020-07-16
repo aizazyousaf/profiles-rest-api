ac306524be625713a06a55e26495a34f307351e6
Aizaz <aizaz632@gmail.com>
2020-07-16 15:21:40 +0500

    Added Searching to user Profile API

--- profile_api/views.py
+++ profile_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from profile_api import models
 from profile_api import permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
@@ -104,3 +105,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
